ee8944350b8e0aefde5078b0f1d9e0cbcad9a912
sadeeqdev <boskitechh@gmail.com>
2024-01-18 02:06:12 +0100

    fix: fix typo

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,3 +1,3 @@
-export * from "./modalContent/modalContent";
+export * from "./modalContent/ModalContent";
 export * from "./input/AmountField";
 export * from "./buttons/Button";
--- src/components/modals/index.tsx
+++ src/components/modals/index.tsx
@@ -1,2 +1,2 @@
-export * from "./SupplyModal/SupplyModal";
+export * from "./supplyModal/SupplyModal";
 export * from "./SuccessModal/SuccessModal";
