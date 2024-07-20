9ee6726a675bf57023ebb280267eb6388e62e4a6
sadeeqdev <boskitechh@gmail.com>
2024-07-20 14:04:33 +0100

    fix: fix loading component in stake tab

--- src/app/markets/[poolId]/stake/StakeTab.tsx
+++ src/app/markets/[poolId]/stake/StakeTab.tsx
@@ -71,11 +71,11 @@ const StakeTab = ({
   const updateCard = () => {
     fetchStakingBalance();
     fetchLpTokenBalance();
-    fetchLpAllowance(false);
+    fetchLpAllowance();
     fetchTotalSupply();
     fetchLpStakers();
     fetchTotalStaked();
-    fetchClaimableRewards(false);
+    fetchClaimableRewards();
     fetchPoolStats();
     fetchAccountInfo();
   };
@@ -87,52 +87,11 @@ const StakeTab = ({
     tokenValueLoading ||
     lpStakersLoading ||
     totalStakedLoading ||
+    claimableRewardsLoading ||
+    lpAllowanceLoading ||
     totalSupplyLoading;
 
-  function getLoadingStates(): string[] {
-    const loadingStates: string[] = [];
-
-    if (lpAllowanceLoading) {
-      loadingStates.push("lpAllowanceLoading");
-    }
-
-    if (lpSymbolLoading) {
-      loadingStates.push("lpSymbolLoading");
-    }
-
-    if (lpAssetLoading) {
-      loadingStates.push("lpAssetLoading");
-    }
-
-    if (lpDecimalsLoading) {
-      loadingStates.push("lpDecimalsLoading");
-    }
-
-    if (tokenValueLoading) {
-      loadingStates.push("tokenValueLoading");
-    }
-
-    if (lpStakersLoading) {
-      loadingStates.push("lpStakersLoading");
-    }
-
-    if (totalStakedLoading) {
-      loadingStates.push("totalStakedLoading");
-    }
-
-    if (claimableRewardsLoading) {
-      loadingStates.push("claimableRewardsLoading");
-    }
-
-    if (totalSupplyLoading) {
-      loadingStates.push("totalSupplyLoading");
-    }
-
-    return loadingStates;
-  }
-
   if (isLoading) {
-    console.log(getLoadingStates());
     return (
       <div className="mt-8 w-full flex space-x-5" data-testid="stake-container">
         <div
