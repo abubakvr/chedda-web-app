0b5d6ee46409a91a9a6fbf97e712a613420016ef
sadeeqdev <boskitechh@gmail.com>
2025-04-17 20:11:13 +0100

    fix: change bot name

--- src/components/common/chatBot/ChatBot.tsx
+++ src/components/common/chatBot/ChatBot.tsx
@@ -11,7 +11,7 @@ export const ChatBot = () => {
         "https://chedda.app.n8n.cloud/webhook/7bc4e919-195e-4109-8663-35cecfe8a484/chat",
       initialMessages: [
         "Eyy, welcome to the Family! 👋🧀",
-        "I'm Don Nathan, your consigliere in the world of DeFi. What can I help you with today?",
+        "I'm Don Giovani, your consigliere in the world of DeFi. What can I help you with today?",
       ],
     });
   }, []);
